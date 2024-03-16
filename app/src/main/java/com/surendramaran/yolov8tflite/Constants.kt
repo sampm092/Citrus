132c1607e3ce4054fb4aa99ee2d6bd2f5c184d93
SamPM092 <sammich4294@gmail.com>
2024-03-16 13:43:46 +0700

    Initial commit

--- app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
+++ app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -1,6 +1,7 @@
 package com.surendramaran.yolov8tflite
 
 object Constants {
-    const val MODEL_PATH = "best_float160803.tflite"
+    const val MODEL_PATH = "best_float160203.tflite"
     const val LABELS_PATH = "labels.txt"
 }
+
